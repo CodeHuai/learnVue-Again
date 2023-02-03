d62262aaf6109e37179783317262e213eab5bb5e
zwz <zouweizhe@guoxintianyu.com>
2023-02-03 14:54:35 +0800

    构建下组件

--- vue-cli-dir/src/App.vue
+++ vue-cli-dir/src/App.vue
@@ -1,45 +1,16 @@
 <template>
-  <div class="container">
-    <button @click="isShow = !isShow">测试</button>
-    <Transition>
-      <HelloWorld v-if="isShow" />
-    </Transition>
-  </div>
+  <transform></transform>
 </template>
 
 <script>
-import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import transform from './components/transform/transform.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    transform
   },
-  setup() {
-    const isShow = ref(false)
-    return {
-      isShow
-    }
-  }
 }
 </script>
 
-<style>
-.container {
-  height: 400px;
-  overflow: hidden;
-}
-
-.v-enter-active {
-  transition: all 3s ease;
-}
-
-.v-enter-from {
-  transform: translateX(100%);
-}
-
-.v-leave-to {
-  transform: translateX(100%);
-}
-</style>
+<style></style>
---
+++ vue-cli-dir/src/components/transform/transform.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <button @click="isShow = !isShow">测试</button>
+    <Transition>
+      <div v-if="isShow" class="pic-container">
+        <img src="../../assets/img/logo.png">
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'HelloWorld',
+  setup() {
+    const isShow = ref(false)
+    return {
+      isShow
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  position: relative;
+  height: 100%;
+}
+.v-enter-active {
+  transition: all 3s ease;
+}
+
+.v-enter-from {
+  transform: translateX(100%);
+}
+
+.v-leave-to {
+  transform: translateX(100%);
+}
+
+.pic-container {
+  position: absolute;
+  overflow: hidden;
+  width: 200px;
+  height: 200px;
+  right: 10px;
+  background-color: aquamarine;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.3s;
+}
+
+img {
+  width: 100px;
+}
+</style>
