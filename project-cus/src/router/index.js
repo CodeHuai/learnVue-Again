03ce7a5a47d114133dde02ffccb66d9561b49156
zwz <zouweizhe@guoxintianyu.com>
2023-02-07 13:59:11 +0800

    init project

---
+++ project-cus/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/home",
+      name: "home",
+      component: () => import("../views/home/home.vue"),
+    },
+    {
+      path: "/favor",
+      name: "favor",
+      component: () => import("@/views/favor/favor.vue"),
+    },
+    {
+      path: "/message",
+      name: "message",
+      component: () => import("@/views/message/message.vue"),
+    },
+    {
+      path: "/order",
+      name: "order",
+      component: () => import("@/views/order/order.vue"),
+    },
+  ],
+});
+
+export default router;
