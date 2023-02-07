91857f389ed281c9d5226c1ce3be31bd0e33c9f0
zwz <zouweizhe@guoxintianyu.com>
2023-02-07 13:59:11 +0800

    修改下项目结构

--- vue-cli-dir/src/main.js
+++ vue-cli-dir/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/index";
+import {createPinia} from "pinia";
 
-import './assets/css/reset.css';
 
-createApp(App).use(router).mount('#app')
+// import './assets/css/reset.css';
+
+createApp(App).use(router).use(createPinia).mount('#app')
--- vue-cli-dir/src/router/route.js
+++ vue-cli-dir/src/router/route.js
@@ -1,5 +1,9 @@
 export const routes = [
   {
+    path: '/',
+    redirect: "/helloWorld"
+  },
+  {
     name: 'HelloWorld',
     path: '/helloWorld',
     component: () => import('../view/transform/transform.vue')
